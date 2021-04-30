735ff96fc00f22ee79b69764dfb7498d1c41dbdd
Prayant <prayantgupta15@gmail.com>
2021-04-30 19:35:37 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
-export default function App() {
+// const MyData = () => {
+//   return <Text>MyData</Text>
+// }
+
+const MyData = (props) => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>{props.name}</Text>
+      <Text>{props.qual}</Text>
     </View>
-  );
+  )
 }
+
+class App extends React.Component {
+  state =
+    {
+      name: "prayant",
+      qual: "school"
+    }
+
+
+  changeState(name, qual) {
+    console.log('Studying ' + name)
+    Alert.alert('Now, you are graduate')
+    this.setState({
+      name: name
+      , qual: qual
+    })
+
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(data => data.json())
+      .then(data2 => {
+        console.log(data2)
+
+        this.setState({ name: data2[0].name })
+      })
+
+  }
+  render() {
+    const items = ['eat', 'sleep', 'code'];
+    const disp = items.map((i) => {
+      return (<Text style={{ fontSize: 30 }}>{i}</Text>)
+    });
+    return (
+      <View style={styles.container}>
+        {/* {disp} */}
+        <MyData name={this.state.name}
+          qual={this.state.qual} />
+        <StatusBar style="auto" />
+        <Button title="Graduate Me"
+          // onPress={() => { console.log('pressed') }
+
+          // onPress={this.changeState}
+          // changeState=()=> {
+          //   console.log('pressed')
+          // }
+
+
+          onPress={this.changeState.bind(this, "Eng. prayant", "btech")}
+        ></Button>
+
+
+        <TextInput
+          placeholder="Enter Text"
+          style={{
+            fontSize: 30,
+            height: 30
+          }}
+          onChangeText={(text) => { Alert.alert(text) }}
+
+        ></TextInput>
+      </View>
+    );
+  }
+}
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
   },
 });
